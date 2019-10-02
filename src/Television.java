15a6d2df256fedef4f4683e4feb2460eced2c9f0
sagarvadalia <sv344@cornell.edu>
2019-10-02 19:12:06 -0400

    comments

--- src/Television.java
+++ src/Television.java
@@ -1,17 +1,23 @@
+//Sagar Vadalia, Saud Millwala, Mack Derly Fonrose, Steven Salmon
 public class Television {
+    //data values
     private String brand;
     private double price;
+    //default constructor
     public Television(){
         this.brand = "undefined brand";
         this.price = 0.0;
     }
+    //overloaded constructor
     public Television(String brand, double price){
         this.brand = brand;
         this.setPrice(price);
     }
+    //price accessor
     double getPrice(){
         return this.price;
     }
+    //price mutator
     void setPrice(double price){
         if(price > 0.0){
             this.price = price;
@@ -20,15 +26,19 @@ public class Television {
             this.price = 0.0;
         }
     }
+    //brand accessor
     public String getBrand(){
         return this.brand;
     }
+    //brand mutator
     public void setBrand(String brand){
         this.brand = brand;
     }
+    //to string: returns brand and price
     public String toString(){
         return "the brand is: " + this.brand + " and the price is: " + this.price;
     }
+    //equals: compares brand and price for equality. returns true or false
     public boolean equals(Television otherTelevision){
         if(this.brand == otherTelevision.brand && this.price == otherTelevision.price){
             return true;
--- src/televisionClient.java
+++ src/televisionClient.java
@@ -1,20 +1,28 @@
 public class televisionClient {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //test default constructor
         Television testTele = new Television();
+        //test accessors for default constructor
         System.out.println("default constructor sets the brand to: " + testTele.getBrand());
         System.out.println("default constructor sets the price to: " + testTele.getPrice());
+        //check to string for default constructor
         System.out.println("our default tele is: " + testTele.toString());
+        //check mutators for default constructor
         testTele.setPrice(5.0);
         System.out.println("our tv now costs: " + testTele.getPrice());
         testTele.setBrand("Panasonic");
         System.out.println("our tv now has a brand of: " + testTele.getBrand());
+        //check overloaded constructor
         Television testTeleTwo = new Television("Samsung", 20.0);
-        System.out.println("overloaded constructor sets the brand to: " + testTele.getBrand());
-        System.out.println("overloaded constructor sets the price to: " + testTele.getPrice());
-        System.out.println("our overloaded tele is: " + testTele.toString());
+        //accessors for overloaded constructor
+        System.out.println("overloaded constructor sets the brand to: " + testTeleTwo.getBrand());
+        System.out.println("overloaded constructor sets the price to: " + testTeleTwo.getPrice());
+        //check tostring and equals
+        System.out.println("our overloaded tele is: " + testTeleTwo.toString());
         System.out.println("are the teles equal?:" + testTele.equals(testTeleTwo));
+        //check mutators for overloaded constructor
         testTeleTwo.setBrand("Panasonic");
         testTeleTwo.setPrice(5.0);
         System.out.println("they should be equal now: " + testTele.equals(testTeleTwo));
